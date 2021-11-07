4baa0987b3aed305f616850163b99cd3a715eb94
Ramesh Verma <rsverma333@gmail.com>
2021-11-07 12:08:16 +0530

    Added ant design based project

--- Collabile.Web/Program.cs
+++ Collabile.Web/Program.cs
@@ -1,12 +1,9 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using AntDesign.ProLayout;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Collabile.Web
 {
@@ -18,6 +15,8 @@ namespace Collabile.Web
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddAntDesign();
+            builder.Services.Configure<ProSettings>(builder.Configuration.GetSection("ProSettings"));
 
             await builder.Build().RunAsync();
         }
